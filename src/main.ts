829099b7e9ff0b67868ccc59d7299d9d4c71e6eb
Jonas Morales <jonasjosuemo2021@outlook.com>
2022-09-06 14:10:16 -0600

    map component works

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl';
+
+Mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXNjcmlwdHN2IiwiYSI6ImNsN2FsdWE2eTAwZDYzdnI1YmZ6M254MHYifQ.pL6-qhgtF_7r9fJ5Bqd14w';
+
 if (environment.production) {
   enableProdMode();
 }
